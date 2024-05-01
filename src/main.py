7e30e1c7f0747b9e5dfca3659c4bf1cfbfe3079e
ger <german.girod@accelone.com>
2024-05-01 08:54:39 -0400

    commit proyect to repo

---
+++ src/main.py
@@ -0,0 +1,92 @@
+from flask import Flask, request
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains import ConversationalRetrievalChain
+from langchain_chroma import Chroma
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
+from langchain.vectorstores import FAISS
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+
+documents = []
+doc_one_loader = PyPDFLoader("")
+doc_two_loadder = PyPDFLoader("")
+doc_three_loader = PyPDFLoader("")
+doc_four_loader = PyPDFLoader("")
+documents.extend(doc_one_loader.load())
+documents.extend(doc_two_loadder.load())
+documents.extend(doc_three_loader.load())
+documents.extend(doc_four_loader.load())
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len
+    )
+chunks = get_text_chunks(get_pdf_text(["Resultados Fecha 1 General.pdf", "Aviso de Regata Cuba Wingfoil Cup 2024.pdf", "Resultados Fecha 1 Avanzados.pdf", "Resultados Fecha 1 intermedio.pdf"]))
+store = get_vectorstore(chunks)
+pdf_qa = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(temperature=0, model_name='gpt-4'),
+    retriever=store.as_retriever(search_kwargs={'k': 6}),
+    return_source_documents=True,
+    verbose=False
+)
+
+chat_history = []
+
+@app.route("/test", methods=["POST"])
+def message():
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+    twilio_phone_number = os.getenv('TWILIO_PHONE_NUMBER')
+    sender_phone_number = request.values.get('From', '')
+    messageType = request.values.get('MessageType')
+    if messageType == 'text':
+        user_question = request.values.get('Body')
+        result = pdf_qa.invoke({"question": user_question, "chat_history": chat_history})
+        chat_history.append((user_question, result["answer"]))
+        message = client.messages.create(
+                body= result["answer"],
+                from_=twilio_phone_number,
+                to=sender_phone_number
+        )
+        return str(message.sid)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
